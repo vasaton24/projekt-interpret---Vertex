97d8d54c5c0169b7b9740d359abc10653cc07f54
vasaton24 <vasaton24@sps-prosek.cz>
2026-03-24 20:30:32 +0100

    Add files via upload

---
+++ src/vertex/exceptions.py
@@ -0,0 +1,11 @@
+class VertexError(Exception):
+    """Základní třída pro chyby v jazyce Vertex."""
+    pass
+
+class VertexSyntaxError(VertexError):
+    """Chyba v zápisu kódu."""
+    pass
+
+class VertexRuntimeError(VertexError):
+    """Chyba při běhu (např. dělení nulou)."""
+    pass
