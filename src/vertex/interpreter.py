d41fc30d452d75ae6b39ffd9102b6197415a7dca
vasaton24 <vasaton24@sps-prosek.cz>
2026-03-24 22:14:46 +0100

    Create interpreter.py

---
+++ src/vertex/interpreter.py
@@ -0,0 +1,49 @@
+from .exceptions import VertexRuntimeError, VertexSyntaxError
+
+class Interpreter:
+    def __init__(self, env, output_widget):
+        self.env = env
+        self.output_widget = output_widget
+
+    def execute(self, tokens):
+        i = 0
+        while i < len(tokens):
+            t = tokens[i]
+            if t.type == "VAR":
+                # var x = 10 + 5 ;
+                name = tokens[i+1].value
+                end = i
+                while end < len(tokens) and tokens[end].type != "SEMI":
+                    end += 1
+               
+                expr_tokens = tokens[i+3 : end]
+                val = self.evaluate_expression(expr_tokens)
+                self.env.set(name, val)
+                i = end + 1
+            elif t.type == "PRINT":
+                val = self.env.get(tokens[i+1].value) if tokens[i+1].type == "ID" else int(tokens[i+1].value)
+                self.output_widget.insert("end", f"> {val}\n")
+                i += 3
+            else:
+                i += 1
+
+    def evaluate_expression(self, tokens):
+        if not tokens: return 0
+        res = self.get_value(tokens[0])
+        idx = 1
+        while idx < len(tokens):
+            op = tokens[idx].value
+            next_val = self.get_value(tokens[idx+1])
+            if op == "+": res += next_val
+            elif op == "-": res -= next_val
+            elif op == "*": res *= next_val
+            elif op == "/":
+                if next_val == 0: raise VertexRuntimeError("Division by zero!")
+                res //= next_val
+            idx += 2
+        return res
+
+    def get_value(self, token):
+        if token.type == "NUMBER": return int(token.value)
+        if token.type == "ID": return self.env.get(token.value)
+        raise VertexSyntaxError(f"Unexpected token: {token.value}")
