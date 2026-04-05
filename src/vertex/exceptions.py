c8739959e4dc8672e389e0a24e543babf58ffa89
vasaton24 <vasaton24@sps-prosek.cz>
2026-04-05 20:02:11 +0200

    Update exceptions.py

--- src/vertex/exceptions.py
+++ src/vertex/exceptions.py
@@ -1,11 +1,8 @@
 class VertexError(Exception):
-    """Základní třída pro chyby v jazyce Vertex."""
     pass
 
 class VertexSyntaxError(VertexError):
-    """Chyba v zápisu kódu."""
     pass
 
 class VertexRuntimeError(VertexError):
-    """Chyba při běhu."""
     pass
